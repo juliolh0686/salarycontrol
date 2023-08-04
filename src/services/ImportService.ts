2b3a28d46d594a46a6dabf2af93ba4cc6c029c69
juliolh0686 <juliolh06@gmail.com>
2023-08-04 18:26:18 -0500

    update import002

--- src/services/ImportService.ts
+++ src/services/ImportService.ts
@@ -32,6 +32,32 @@ async fetchfile(file:File,tipocarga:string) {
 
   }
 
+  async import002activos(file:File,tipocarga:string) {
+
+    let formData = new FormData();
+    formData.append('file',file);
+    formData.append('tipocarga',tipocarga)
+  
+      let store = useAuth()
+      const url = store.baseURL+'/planillaconceptos/import002rem'
+      
+      const rawResponse = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      })
+  
+      const response = await rawResponse.json();
+  
+      if(response.status == false) {
+        alert(response.message)
+        return false
+      }else {
+        alert(response.res)
+        return true
+      }
+  
+    }
+
 }
 
 export default ImportService;
--- src/views/ImportacionView.vue
+++ src/views/ImportacionView.vue
@@ -10,7 +10,23 @@
     </div>
     <input type="file" id="file" name="file" ref="fileInput" @change="handleFileUpload()"/>
     <button v-on:click="submitFile()">Importar</button>
+
+
+    <div class="import-002">
+      <select class="form-control" v-model="tipocarga002">
+        <option disabled value="">Seleccionar</option>
+        <option value="1">CONTINUA</option>
+        <option value="2">CONTINUA-B</option>
+        <option value="3">OCASIONAL</option>
+        <option value="4">COMPLEMENTARIA</option>
+      </select>
+      <input type="file" id="file002" name="file002" ref="fileInput002" @change="handleFileUpload002()"/>
+      <button v-on:click="submitFile002()">Importar</button>
+    </div>
+    
+    
   </div>
+  
 </template>
 
 <script lang="ts" setup>
@@ -20,10 +36,14 @@
   import useAuth from "@/store/auth"
 
   const fileInput = ref<HTMLInputElement | null>(null);
+  const fileInput002 = ref<HTMLInputElement | null>(null);
   const files = ref()
+  const files002 = ref()
   const file = ref()
+  const file002 = ref()
   const store = new ImportService()
   let tipocarga = ref('')
+  let tipocarga002 = ref('')
 
   const handleFileUpload = () => {
     files.value = fileInput.value?.files
@@ -34,9 +54,27 @@
 
   }
 
+  const handleFileUpload002 = () => {
+    files002.value = fileInput002.value?.files
+    file002.value = files002.value[0]
+  }
+
+
   const submitFile = async() => {
     
     const response = await store.fetchfile(file.value,tipocarga.value);
+
+    if (response == false) {
+      console.log("Error en carga")
+    } else {
+      console.log("Satisfactorio")
+    }
+
+  }
+
+  const submitFile002 = async() => {
+    
+    const response = await store.import002activos(file002.value,tipocarga002.value);
 
     if (response == false) {
       console.log("Error en carga")
